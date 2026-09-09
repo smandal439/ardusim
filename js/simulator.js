92a008d554c621613e406edf5d7d100be01c953e
SUSHANTA <SUSHANTA@local>
2026-09-09 23:03:26 +0530

    feat: add DIP Switch and LED Array example circuit and update simulator to include it

--- js/simulator.js
+++ js/simulator.js
@@ -1716,7 +1716,7 @@ window.loadExamplesFromFiles = async function () {
     'opamp_741_non_inverting', 'vl53l0x_proximity_sensor', 'esp32_i2s_music_player',
     'esp32_i2s_local_radio_player', 'lcd', 'read_rfid_card_raw_data', 'lcd_print_remotely',
     'rfid_inventory_tracker','shift_resister_circuit','7408_test_with_logic_analyzer',
-    'espnow_sender','espnow_receiver','coap_client','espnow_led_control','two_lcd','esp_now_dip_switch_to_8_led'];
+    'espnow_sender','espnow_receiver','coap_client','espnow_led_control','two_lcd','esp_now_dip_switch_to_8_led','dip_switch_and_led_array'];
   const sketches = [];
   const cacheBust = '?v=' + Date.now();
   for (const name of files) {
