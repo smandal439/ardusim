9faf7e16f889efcd9e9e863f593455fcf827bdd7
SUSHANTA <SUSHANTA@local>
2026-09-15 00:03:19 +0530

    chore: remove temp debug scripts

--- _grep.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const src = fs.readFileSync('js/simulator.js', 'utf8');
-const lines = src.split('\n');
-lines.forEach((l, i) => {
-  if (l.includes('new RegExp'))
-    console.log('Line ' + (i+1) + ': ' + l.trim().substring(0, 150));
-});
